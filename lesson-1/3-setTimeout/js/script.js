a9c19f2607662853918cbe792960c166e2acab15
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-01-31 14:07:43 +0100

    s

--- lesson-1/3-setTimeout/js/script.js
+++ lesson-1/3-setTimeout/js/script.js
@@ -1,2 +1,10 @@
 // The setTimeout method is used to execute a
 // function after a specified period of time.
+setTimeout(function(){
+console.log("buuuh")
+}, 1000);
+
+var loaded = document.querySelector(".loader");
+setTimeout(function(){
+    loaded.innerHTML = "finnished downloading, mother fu%&#!";
+},4000);
